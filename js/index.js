8b5280652c3c13211da7b003ee112d44621260db
Andrei Bondarev <andrei.bondarev13@gmail.com>
2015-03-28 17:29:24 -0400

    Highlight/unhighlight functions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var searchableMall = angular.module('SearchableMall', []);
 
 searchableMall.constant('BG_HIGHLIGHT', 'yellow');
+searchableMall.constant('TEXT_HIGHLIGHT', 'black');
 searchableMall.constant('PRODUCT_DATA', [{
 		store: 'Nordstrom',
 		keywords: ['north face', 'columbia']
@@ -18,12 +19,16 @@ searchableMall.run(function() {
 
 });
 
-searchableMall.controller('SearchableController', ['$scope', 'PRODUCT_DATA', function($scope, PRODUCT_DATA) {
+searchableMall.controller('SearchableController', ['$scope', 'PRODUCT_DATA', 'BG_HIGHLIGHT', 'TEXT_HIGHLIGHT', function($scope, PRODUCT_DATA, BG_HIGHLIGHT, TEXT_HIGHLIGHT) {
 	$scope.products = PRODUCT_DATA;
 
 	$scope.results = [];
 
 	$scope.$watch('query', function(newVal, oldVal) {
+		if ($scope.results.length > 0) {
+			unhighlight($scope.results);
+		}
+
 		var results = _.filter(PRODUCT_DATA, function(item) {
 			return _.find(item.keywords, function(keyword) {
 				return ~keyword.indexOf(newVal);
@@ -31,11 +36,32 @@ searchableMall.controller('SearchableController', ['$scope', 'PRODUCT_DATA', fun
 		});
 
 		$scope.results = results;
-		$scope.results_count = $scope.results.length
+		$scope.results_count = $scope.results.length;
+
+		_.each(results, function(result) {
+			highlight(result.name);
+		});
 	});
 
 	function highlight(result) {
-		$('').css('fill', )
+		_.each($('#' + result).children(), function(child) {
+			child.style.fill = BG_HIGHLIGHT;
+		});
+
+		_.each($('#' + result + ' > text').children(), function(child) {
+			child.style.fill = TEXT_HIGHLIGHT;
+		});
 	}
 
+	function unhighlight(results) {
+		_.each(results, function(result) {
+			_.each($('#' + result.name).children(), function(child) {
+				child.style.fill = '';
+			});
+
+			_.each($('#' + result.name + ' > text').children(), function(child) {
+				child.style.fill = '';
+			});		
+		});
+	}
 }]);
