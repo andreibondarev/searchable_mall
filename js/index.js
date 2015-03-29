ee01939d1c35c6c97bfdd301bb393f10628da3ae
mikob <fxrock2002@yahoo.com>
2015-03-29 00:39:03 -0400

    dks

--- js/index.js
+++ js/index.js
@@ -2,28 +2,55 @@ var searchableMall = angular.module('SearchableMall', ['ngAnimate']);
 
 searchableMall.constant('BG_HIGHLIGHT', 'yellow');
 searchableMall.constant('TEXT_HIGHLIGHT', 'black');
-searchableMall.constant('PRODUCT_DATA', [{
-	    id: 'nordstrom',
-        store: 'Nordstrom',
-		keywords: ['north', 'face', 'jackets', 'columbia'],
-        details: 'Nordstrom Details',
-        image: 'images/lids.jpg'
+searchableMall.constant('PRODUCT_DATA', [
+	{
+		id: 'icing_by_claires',
+		store: 'Icing by Claire\'s',
+		details: "ICING strives to show you our latest and greatest products as you’re surfing our site. However, we can’t always guarantee that the products we show online will be available in all stores at all times. For more...",
+		hours: 'Monday to Saturday 10am - 9:30pm, Sunday 11am - 6pm',
+		categories: ['Accessories', 'Cosmetics', 'Jewelry'],
+		keywords: [],
+		image: 'images/icing_by_claires.jpg'
+	},
+	{
+    	id: 'henri_bendel',
+    	store: 'Henri Bendel',
+    	hours: 'Monday to Saturday 10am - 9:30pm, Sunday 11am - 6pm',
+    	details: "Henri Bendel is a girls' playground, where Bendel girls go to find the new and the next in accessories, cosmetics and gifts.  Since 1895, signature brown and white stripe Henri Bendel shopping bags have been spotted...",
+    	categories: ["Women's Fashions", "Accessories", "Women's Specialty"],
+    	keywords: [],
+    	image: 'images/henri_bendel.jpg'
+    },
+	{
+	    id: 'free_people',
+	    store: 'Free People',
+	    hours: 'Monday to Saturday 10am - 9:30pm, Sunday 11am - 6pm',
+        details: 'The Free People woman lives free through fashion, art, music, and travel. She is a free spirit, pushing the limits in the name of adventure. The eclectic look consists of quality apparel, shoes, and accessories that...',
+        categories: ["Women's Fashions"],
+        keywords: [],
+        image: 'images/free_people.jpg'
     }, {
-        id: 'macys',
-		store: 'Macy\'s',
-		keywords: ['columbia', 'north', 'face', 'coats'],
-        details: 'Macy\'s Details',
-        image: 'images/lids.jpg'
+        id: 'francescas_collections',
+		store: 'Francesca\'s Collection',
+		hours: 'Monday to Saturday 10am - 9:30pm, Sunday 11am - 6pm',
+        details: "Francesca's specializes in the most fashion-forward styles in women's clothing, accessories, and gifts. Our stores are constantly filled with new treasures and our style is always one step ahead of the trend. Each...",
+        categories: ['Jewelry & Watches', "Women's Fashions", "Accessories"],
+		keywords: [],
+        image: 'images/francescas_collections.jpg'
     }, {
-        id: 'capital-one-bank',
-		store: 'Capital One Bank',
-		keywords: ['currency'],
-        details: 'Capital One Bank Details',
-        image: 'images/lids.jpg'
+    	id: 'accessorize',
+    	store: 'Accessorize',
+		hours: "Monday to Saturday 10am - 9:30pm, Sunday 11am - 6pm",
+		categories: ['Accessories', '‎Outlet', '‎Bags', '‎Jewellery'],
+        details: 'Accessorize is devoted to bringing the most exciting and eclectic products to the high street. With its own in-house design team, Accessorize holds a unique position on the high street with its inspirational, globally...',
+        keywords: [],
+        image: 'images/accessorize.jpg'
     }, {
     	id: 'lids',
     	store: 'Lids',
-		details: "The LIDS Sports Group, operating within Hat World, Inc., comprises the LIDS retail headwear stores, the LIDS Locker Room specialty fan retail chain, the LIDS Clubhouse retail stores, the LIDS Team Sports wholesale team sports business and its Internet businesses, www.lids.com, www.lids.ca, and www.lidsteamsports.com. Operating out of Indianapolis, Indiana, the retail businesses make up more than 975 mall-based, airport, street level and factory outlet locations nationwide, and in Canada and Puerto Rico. LIDS retail stores offer officially-licensed and branded college, major professional sports teams, as well as other specialty fashion categories all in the latest styles and colors. LIDS Locker Room is a mall-based retailer of sports headwear, apparel, accessories, and novelties. Most LIDS and LIDS Locker Room stores also offer custom embroidery capability. LIDS Clubhouse operates team-specific professional sports and university athletics retail stores and e-commerce websites. LIDS Team Sports is a full-service team uniform and apparel dealer, custom screen printer, embroidery and sporting goods distributor. Hat World, Inc. is a subsidiary of Genesco Inc.",
+    	hours: 'Monday to Saturday 10am - 9:30pm, Sunday 11am - 6pm',
+		details: "Lids is the world's largest retailer specializing in the sale of officially licensed and branded headwear. Every store offers a vast assortment of college, MLB, NBA, NFL and NHL teams, as well as other specialty fashion...",
+		categories: ["Accessories", "Sports Fashion", "Headwear"],
 		keywords: [],
         image: 'images/lids.jpg'
     }
