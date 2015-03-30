af59c4c68f4e215154d19fd1beec742a94bf93c7
Andrei Bondarev <andrei.bondarev13@gmail.com>
2015-03-29 23:37:22 -0400

    Remove debugger

--- js/index.js
+++ js/index.js
@@ -9,104 +9,103 @@ searchableMall.run(function() {
 });
 
 searchableMall.directive('autofocus', ['$interval', function($interval) {
-    return {
-        restrict: 'A',
-        link: function(scope, elem, attr, ctrl) {
-        	$interval(function() {
-	            elem.focus();
-        	}, 200);
-        }
-    };
+  return {
+    restrict: 'A',
+    link: function(scope, elem, attr, ctrl) {
+      $interval(function() {
+        elem.focus();
+      }, 200);
+    }
+  };
 }]);
 
 searchableMall.controller('SearchableController', ['$scope', 'PRODUCT_DATA', 'BG_HIGHLIGHT', 'TEXT_HIGHLIGHT', function($scope, PRODUCT_DATA, BG_HIGHLIGHT, TEXT_HIGHLIGHT) {
-	$scope.products = PRODUCT_DATA;
+  $scope.products = PRODUCT_DATA;
 
-	$scope.results = [];
-	$scope.queryWords = [];
-	var oldResults = [];
+  $scope.results = [];
+  $scope.queryWords = [];
+  var oldResults = [];
 
-	$(document).on('keydown', function(ev) {
-	    if (ev.keyCode == 27) {
-	    	$scope.query = '';
-	    	$scope.showPanel = false;
-			$scope.panelObject = null;
-	    }
-	});
-
-
-    $scope.openDetails = function(item) {
-    	if (item.details && item.hours) {
-	        $scope.panelObject = item;
-	        $scope.showPanel = true;
-        }
+  $(document).on('keydown', function(ev) {
+    if (ev.keyCode == 27) {
+      $scope.query = '';
+      $scope.showPanel = false;
+      $scope.panelObject = null;
     }
+  });
 
-	$scope.$watch('query', function(newVal, oldVal) {
-		var newVal = newVal.toLowerCase();
-		$scope.showPanel = false;
-		$scope.panelObject = null;
 
-		_.each(oldResults, function(oldResult) {
-			highlight(oldResult, true);
-		});
+  $scope.openDetails = function(item) {
+    if (item.details && item.hours) {
+      $scope.panelObject = item;
+      $scope.showPanel = true;
+    }
+  }
 
-		if (newVal) {
-			$scope.results = _.filter(PRODUCT_DATA, function(item) {
-				var keywords = item.keywords.concat(item.store.toLowerCase(), item.id);
-				$scope.queryWords = newVal.split(' ');
+  $scope.$watch('query', function(newVal, oldVal) {
+    var newVal = newVal.toLowerCase();
+    $scope.showPanel = false;
+    $scope.panelObject = null;
 
-				item.score = _.reduce(keywords, function(sum, keyword) {
-					return _.reduce($scope.queryWords, function(sum, queryWord) {
-						return sum + (~keyword.toLowerCase().indexOf(queryWord) ? 1 : 0);
-					}, sum);
-				}, 0);
+    _.each(oldResults, function(oldResult) {
+        highlight(oldResult, true);
+    });
 
-				return !!item.score;
-			});
-			angular.copy($scope.results, oldResults);
+    if (newVal) {
+      $scope.results = _.filter(PRODUCT_DATA, function(item) {
+        var keywords = item.keywords.concat(item.store.toLowerCase(), item.id);
+        $scope.queryWords = newVal.split(' ');
 
-			_.each($scope.results, function(result) {
-				debugger
-				if (result.score >= $scope.queryWords.length) {
-					highlight(result);
-				}
-			});
-		} else {
-			$scope.results = []
-		}
-	});
+        item.score = _.reduce(keywords, function(sum, keyword) {
+          return _.reduce($scope.queryWords, function(sum, queryWord) {
+            return sum + (~keyword.toLowerCase().indexOf(queryWord) ? 1 : 0);
+          }, sum);
+        }, 0);
 
-	$scope.greaterThanOrEqual = function(prop, val){
-	    return function(item){
-	      return item[prop] >= val;
-	    }
-	};
+        return !!item.score;
+      });
+      angular.copy($scope.results, oldResults);
 
-	function highlight(result, undo) {
-		try {
-			_.each($('#' + result.id).children(), function(child) {
-				if (undo) {
-					child.style.fill = '';
-				} else {
-					child.style.fill = BG_HIGHLIGHT;
-				}
-			});
-		} catch(e) {
+      _.each($scope.results, function(result) {
+        if (result.score >= $scope.queryWords.length) {
+          highlight(result);
+        }
+      });
+    } else {
+      $scope.results = []
+    }
+  });
 
-		}
+    $scope.greaterThanOrEqual = function(prop, val){
+      return function(item){
+        return item[prop] >= val;
+      }
+    };
 
-		try {
-			_.each($('#' + result.id + ' > text').children(), function(child) {
-				if (undo) {
-					child.style.fill = '';
-				} else {
-					child.style.fill = TEXT_HIGHLIGHT;
-				}
-			});
-		} catch (e) {
+    function highlight(result, undo) {
+      try {
+        _.each($('#' + result.id).children(), function(child) {
+          if (undo) {
+            child.style.fill = '';
+          } else {
+            child.style.fill = BG_HIGHLIGHT;
+          }
+        });
+      } catch(e) {
 
-		}
-	}
+      }
+
+      try {
+        _.each($('#' + result.id + ' > text').children(), function(child) {
+          if (undo) {
+            child.style.fill = '';
+          } else {
+            child.style.fill = TEXT_HIGHLIGHT;
+          }
+        });
+      } catch (e) {
+
+      }
+    }
 
 }]);
